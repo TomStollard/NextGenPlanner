530913f68eb143b41896b4ae16a7db0172054d6b
Tom Stollard <code@tomstollard.me.uk>
2015-08-29 09:32:08 +0100

    Added API auth + session listing

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ module.exports = function(db){
   var express = require("express");
   var router = express.Router();
   var basicAuth = require("basic-auth");
+  var mongojs = require("mongojs");
 
 
   function unauthorised(res){
@@ -28,11 +29,22 @@ module.exports = function(db){
   router.use(function(req, res, next){
     //checks token matches username
     db.sessions.findOne({
-      userid: req.auth.userid,
-      token: req.auth.token
+      userid: mongojs.ObjectId(req.auth.userid),
+      _id: req.auth.token
     }, function(err, session){
+      //check if a session was found and if it is valid
       if(session){
         if((session.expiry == 0) || (session.expiry > Date.now())){
+          if(session.expiry > Date.now()){
+            //if session is browser-based, update expiry tome
+            db.sessions.update({
+              _id: session._id
+            }, {
+              $set: {
+                expiry: Date.now() + 600000
+              }
+            });
+          }
           return next();
         }
         else {
@@ -45,10 +57,12 @@ module.exports = function(db){
     });
   });
 
-  router.get("/", function(req, res){
-    //res.write("Hello World!");
-    res.json(req.auth);
-    //res.end();
+  router.get("/sessions", function(req, res){
+    db.sessions.find({
+      userid: mongojs.ObjectId(req.auth.userid)
+    }, function(err, sessions){
+      res.json(sessions)
+    });
   });
 
   return router;
