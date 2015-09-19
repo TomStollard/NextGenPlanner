49d63272919fbb272eaa09881cad4f8b99b91353
Tom Stollard <code@tomstollard.me.uk>
2015-09-19 09:35:09 +0100

    Mostly completed tometable UI

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,16 @@
 var credentials;
-var tometable = [{"_id":"55f094c4980b359294067ed5","userid":"55e2af6d20ee7908126c260c","deleted":false,"day":0,"week":0,"startperiod":0,"endperiod":0,"subject":"Maths","teacher":"Mr McCrink","room":"23"}];
+var tometable = [{"_id":"55f094c4980b359294067ed5","userid":"55e2af6d20ee7908126c260c","deleted":false,"day":0,"week":1,"startperiod":0,"endperiod":6,"subject":"Maths","teacher":"Mr McCrink","room":"23"}];
 var options = {
   offlinesync: false,
   nextdaytome: [15, 50],
   tometable: {
     mode: "day",
     multiweek: {
-      offset: 1,
+      offset: 0,
       numweeks: 2
     },
     multiday: {
-      offset: -1,
+      offset: 6,
       numdays: 10
     },
     schooldays: [0,1,2,3,4],
@@ -246,7 +246,7 @@ function loaddaydetails(callback){
   while((lessons.length < 1) && (i < 30)){
     daydate.add(1, "days");
     lessons = dbdata.tometable.findondate(tometable, daydate.toDate());
-    dayname = daydate.format("dddd do");
+    dayname = daydate.format("dddd Do");
     i++;
   }
 
@@ -259,7 +259,6 @@ function loaddaydetails(callback){
     homework: [],
     dayname: dayname
   }));
-  console.log(lessons);
 
   callback();
 }
@@ -293,7 +292,24 @@ $(".button-global-logout").click(function(){
 
 //start tometable page
 $("#page-tometable").on("load", function(){
-
+  if(options.tometable.mode == "day"){
+    $("#tometablecontainer").html(
+      templates.tometabledaymode({
+        periods: options.tometable.periods,
+        days: dbdata.tometable.sortintodays(dbdata.tometable.addlessonheight(tometable))
+      })
+    );
+  }
+  if(options.tometable.mode == "week"){
+    console.log("HI!");
+    console.log(dbdata.tometable.sortintoweeks(dbdata.tometable.addlessonheight(tometable)));
+    $("#tometablecontainer").html(
+        templates.tometableweekmode({
+          periods: options.tometable.periods,
+          weeks: dbdata.tometable.sortintoweeks(dbdata.tometable.addlessonheight(tometable))
+        })
+    );
+  }
   $(this).trigger("loaded");
 });
 
@@ -384,51 +400,55 @@ var dbdata = {
       });
     },
     findondate: function(tometabledata, date){
-      if(options.tometable.mode == "week"){
-        var weekid = moment(date).diff(moment(345600000), "weeks"); //get number of weeks between date given and 1st monday in 1970
-        var tometableweekid = (weekid + options.tometable.multiweek.offset)%(options.tometable.multiweek.numweeks); //add on offset (to allow user week selection), do mod num of weeks to get current week ID
-        var tometabledayid = moment(date).isoWeekday() - 1; //get day id from
-        var lessons = [];
-        $.each(tometabledata, function(i, lesson){
-          if((lesson.day == tometabledayid) && (lesson.week == tometableweekid)){
-            lessons.push(lesson);
-          }
-        });
-        return lessons;
-      }
-      else if (options.tometable.mode == "day") {
-        var dayinweek = moment(date).isoWeekday() - 1;
-        if(dayinweek in options.tometable.schooldays){
-          var dayid = moment(date).diff(moment(345600000), "days"); //get num of days between first monday in 1970 and date given
-          dayid -= (moment(date).diff(moment(345600000), "weeks") * (7-options.tometable.schooldays.length)); //subtract all days not counted in previous weeks
-          for(var i = 0; i < dayinweek; i++){ //loop through previous days this week, and remove if not included in rotation
-            if(!(i in options.tometable.schooldays)){
-              dayid -= 1;
-            }
-          }
-          dayid += options.tometable.multiday.offset;
-          console.log(dayid);
-          var tometabledayid = dayid%options.tometable.multiday.numdays; //get day id in tometable
-          console.log(tometabledayid);
+      if((moment(date).isoWeekday() - 1) in options.tometable.schooldays){
+        if(options.tometable.mode == "week"){
+          var weekid = moment(date).diff(moment(345600000), "weeks"); //get number of weeks between date given and 1st monday in 1970
+          var tometableweekid = (weekid + options.tometable.multiweek.offset)%(options.tometable.multiweek.numweeks); //add on offset (to allow user week selection), do mod num of weeks to get current week ID
+          console.log(date);
+          console.log(tometableweekid);
+          var tometabledayid = moment(date).isoWeekday() - 1; //get day id from
           var lessons = [];
-          $.each(tometabledata, function(i, lesson){ //loop through lessons, check if on correct day
-            if((lesson.day == tometabledayid)){
+          $.each(tometabledata, function(i, lesson){
+            if((lesson.day == tometabledayid) && (lesson.week == tometableweekid)){
               lessons.push(lesson);
             }
           });
           return lessons;
         }
+        else if (options.tometable.mode == "day") {
+          var dayinweek = moment(date).isoWeekday() - 1;
+          if(dayinweek in options.tometable.schooldays){
+            var dayid = moment(date).diff(moment(345600000), "days"); //get num of days between first monday in 1970 and date given
+            dayid -= (moment(date).diff(moment(345600000), "weeks") * (7-options.tometable.schooldays.length)); //subtract all days not counted in previous weeks
+            for(var i = 0; i < dayinweek; i++){ //loop through previous days this week, and remove if not included in rotation
+              if(!(i in options.tometable.schooldays)){
+                dayid -= 1;
+              }
+            }
+            dayid += options.tometable.multiday.offset;
+            var tometabledayid = dayid%options.tometable.multiday.numdays; //get day id in tometable
+            var lessons = [];
+            $.each(tometabledata, function(i, lesson){ //loop through lessons, check if on correct day
+              if((lesson.day == tometabledayid)){
+                lessons.push(lesson);
+              }
+            });
+            return lessons;
+          }
+          else {
+            return [];
+          }
+        }
         else {
           return [];
         }
       }
-      else {
+      else{
         return [];
       }
     },
     addperiodtomes: function(tometabledata){
       $.each(tometabledata, function(i, lesson){
-        console.log(lesson);
         lesson.starttome = options.tometable.periods[lesson.startperiod].slice(0, 2);
         lesson.endtome = options.tometable.periods[lesson.endperiod].slice(2);
       })
@@ -443,6 +463,57 @@ var dbdata = {
         }
         return 0;
       });
+    },
+    sortintoweeks: function(tometabledata){
+      var weeks = [];
+      for(var i = 0; i < options.tometable.multiweek.numweeks; i++){
+        weeks[i] = [];
+        $.each(options.tometable.schooldays, function(x, dayid){
+          weeks[i][dayid] = [];
+        });
+      }
+      $.each(tometabledata, function(i, lesson){
+        weeks[lesson.week][lesson.day][lesson.startperiod] = lesson;
+      });
+      $.each(weeks, function(x, week){
+        $.each(week, function(y, day){
+          for(var i = 0; i < options.tometable.periods.length; i++){
+            if(day[i]){
+              i = day[i].endperiod;
+            }
+            else {
+              day[i] = false;
+            }
+          }
+        });
+      })
+      return weeks;
+    },
+    sortintodays: function(tometabledata){
+      var days = [];
+      for(var i = 0; i < options.tometable.multiday.numdays; i++){
+        days[i] = [];
+      }
+      $.each(tometabledata, function(i, lesson){
+        days[lesson.day][lesson.startperiod] = lesson;
+      });
+      $.each(days, function(y, day){
+        for(var i = 0; i < options.tometable.periods.length; i++){
+          if(day[i]){
+            i = day[i].endperiod;
+          }
+          else {
+            day[i] = false;
+          }
+        }
+      });
+      return days;
+    },
+    addlessonheight: function(tometabledata){
+      $.each(tometabledata, function(i, lesson){
+        lesson.height = ((lesson.endperiod - lesson.startperiod + 1) * 100) + "px";
+      });
+      return tometabledata;
     }
   }
 }
@@ -452,7 +523,9 @@ var dbdata = {
 var templates = {
   "thisweek": Handlebars.compile($("#template-thisweek").html()),
   "dayview": Handlebars.compile($("#template-dayview").html()),
-  "todo": Handlebars.compile($("#template-todo").html())
+  "todo": Handlebars.compile($("#template-todo").html()),
+  "tometableweekmode": Handlebars.compile($("#template-tometable-weekmode").html()),
+  "tometabledaymode": Handlebars.compile($("#template-tometable-daymode").html())
 }
 
 Handlebars.registerHelper("formatDate", function(datetome, format){
@@ -463,5 +536,10 @@ Handlebars.registerHelper("dateToNow", function(datetome, format){
   return moment(new Date(datetome)).toNow();
 });
 
+Handlebars.registerHelper("humanReadableIndex", function(index){
+  return index + 1;
+});
+
 Handlebars.registerPartial("homeworkitem", $("#template-homeworkitem").html());
+Handlebars.registerPartial("tometable", $("#template-tometable").html());
 // end templates
