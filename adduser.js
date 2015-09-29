0a0d0c3fa00c45bf9d637cae793e29b6f3a270ad
Tom Stollard <code@tomstollard.me.uk>
2015-09-29 10:31:50 +0100

    Added debugging to adduser

--- adduser.js
+++ adduser.js
@@ -5,10 +5,15 @@ var mongojs = require("mongojs"),
 dotenv.load();
 
 var db = mongojs(process.env.DBURL, ["users"]);
+console.log(process.env.DBURL);
 
 bcrypt.hash(process.argv[3], 10, function(err, hash) {
   db.users.insert({
     username: process.argv[2],
     password: hash
+  }, function(err, result){
+    if(err){
+      console.log(err);
+    }
   });
 });
